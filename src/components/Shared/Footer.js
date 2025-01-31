e48e15632c36f8d4aa699df5111203087bcaf8f3
ruabelyan <ru.abelyan@mail.ru>
2025-01-31 13:56:30 +0400

    fix 2024

--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -78,7 +78,7 @@ const Footer = () => {
                   <div className="row">
                      <div className="col-xl-12">
                         <div className="footer-copyright footer-copyright-3 text-center">
-                           <p>Copyright by@ rubabelyan - 2024</p>
+                           <p>Copyright by@ rubabelyan - 2025</p>
                         </div>
                      </div>
                   </div>
